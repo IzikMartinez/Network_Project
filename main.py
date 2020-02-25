ef45ebc68b61d4cfa95338b5f5de5cf71fc31ccf
Mara McGuire <maramcguire@ymail.com>
2020-02-25 14:43:23 -0600

    Added CreateNetwork Function and main function
    Signed-off-by: Mara McGuire <maramcguire@ymail.com>

--- main.py
+++ main.py
@@ -1,14 +1,46 @@
 import networkx as nx
-import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 
-JazzTSV = 'arenas-jazz/out.arenas-jazz'
-Jazz_Read = pd.read_csv(JazzTSV, sep='\t')
 
-print(Jazz_Read.head(10))
+# Create Network: This function is to create a network based on the provided information
+# in the Kangaroo and Bison Network files.
+# Parameters: The Imported, created Graph and The Lines from that Graph
+def CreateNetwork (Graph,File):
+    Opened_File = open(File, 'r')
+    Lines = Opened_File.readlines()
 
-# I found a way to read the TSV file but so far I don't know how to use it correctly so I
-# could have read it incorrectly. If you look at the TSV file for Jazz musicians it has 2 columns
-# I assumed them to be which nodes are connected but I also looked at the Dutch college file
-# and its completely different ruining all my theories.
+    for ln in Lines[2:-1]:
+        Start_node = int(ln.split(' ')[0])
+        End_node = int(ln.split(' ')[1])
+        Edge_length = float(ln.split(' ')[2])
+        Graph.add_edge(Start_node, End_node, weight=Edge_length)
+
+    pos = nx.spring_layout(Graph)
+    # Nodes
+    nx.draw_networkx_nodes(Graph, pos, node_size=50)
+    # Edges
+    nx.draw_networkx_edges(Graph, pos, width=1)
+    plt.show()
+
+# Main Function for the program.
+def main():
+    #Directed Bison Network
+    BisonFile = 'moreno_bison/out.moreno_bison_bison'
+    BisonGraph = nx.DiGraph()
+
+    BisonNetwork = CreateNetwork(BisonGraph,BisonFile)
+
+    #Undirected Kangaroo Network
+    KangarooFile = 'moreno_kangaroo/out.moreno_kangaroo_kangaroo'
+    KangarooGraph = nx.Graph()
+
+    KangaroonNetwork = CreateNetwork(KangarooGraph,KangarooFile)
+
+main()
+
+
+
+
 
 
