f71541bd9739e8ac5b220969da75961305913ffe
Mara McGuire <maramcguire@ymail.com>
2020-02-24 23:58:12 -0600

    Just so you can have it. TSV file read

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
-import networkx
+import networkx as nx
+import pandas as pd
 
-print("Sup Marla")
+JazzTSV = 'arenas-jazz/out.arenas-jazz'
+Jazz_Read = pd.read_csv(JazzTSV, sep='\t')
+
+print(Jazz_Read.head(10))
+
+# I found a way to read the TSV file but so far I don't know how to use it correctly so I
+# could have read it incorrectly. If you look at the TSV file for Jazz musicians it has 2 columns
+# I assumed them to be which nodes are connected but I also looked at the Dutch college file
+# and its completely different ruining all my theories.
+
+
