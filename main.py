f58b297abac72c130fe88fcd387a4cd075df86f6
Mara McGuire <maramcguire@ymail.com>
2020-02-26 01:18:40 -0600

    Part A and Part B just about completed
    Signed-off-by: Mara McGuire <maramcguire@ymail.com>

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 # Create Network: This function is to create a network based on the provided information
 # in the Kangaroo and Bison Network files.
 # Parameters: The Imported, created Graph and The Lines from that Graph
-def CreateNetwork (Graph,File):
+def CreateNetwork (Graph,File,flag):
     Opened_File = open(File, 'r')
     Lines = Opened_File.readlines()
 
@@ -17,6 +17,12 @@ def CreateNetwork (Graph,File):
         Graph.add_edge(Start_node, End_node, weight=Edge_length)
 
     pos = nx.spring_layout(Graph)
+    if flag == True:
+        plt.figure(1)
+        plt.title('Directed Bison graph')
+    else:
+        plt.figure(2)
+        plt.title('Undirected Kangaroo graph')
     # Nodes
     nx.draw_networkx_nodes(Graph, pos, node_size=50)
     # Edges
@@ -29,16 +35,47 @@ def main():
     BisonFile = 'moreno_bison/out.moreno_bison_bison'
     BisonGraph = nx.DiGraph()
 
-    BisonNetwork = CreateNetwork(BisonGraph,BisonFile)
+    BisonNetwork = CreateNetwork(BisonGraph,BisonFile,True)
 
     #Undirected Kangaroo Network
     KangarooFile = 'moreno_kangaroo/out.moreno_kangaroo_kangaroo'
     KangarooGraph = nx.Graph()
 
-    KangaroonNetwork = CreateNetwork(KangarooGraph,KangarooFile)
+    KangaroonNetwork = CreateNetwork(KangarooGraph,KangarooFile,False)
+
+    # Part A: Connected Component Analysis
+    print("Bison Directed Graph Connected Component Analysis",
+          "\nWeakly connected: ", nx.is_weakly_connected(BisonGraph),
+          "\nNumber of Weakly CCs: ", nx.number_weakly_connected_components(BisonGraph),
+          "\nSize of largest CC: ", len(max(nx.weakly_connected_components(BisonGraph), key=len)),
+          "\nSize of smallest CC: ", len(min(nx.weakly_connected_components(BisonGraph), key=len)))
+
+    print("\nKangaroo Undirected Graph Connected Component Analysis",
+          "\nConnected: ", nx.is_connected(KangarooGraph),
+          "\nNumber of CCs: ", nx.number_connected_components(KangarooGraph),
+          "\nSize of largest CC: ", len(max(nx.connected_components(KangarooGraph), key=len)),
+          "\nSize of smallest CC: ", len(min(nx.connected_components(KangarooGraph), key=len)))
+
+    # Part B Computing Degrees and finding the Probability distribution
+    BisonDegrees = []
+    for node in range(1,26):
+        BisonDegrees.append(BisonGraph.degree(node))
+
+    plt.figure()
+    plt.title('Histogram Directed Bison')
+    plt.hist(BisonDegrees,bins = 17)
+
+
+    KangarooDegrees = []
+    for node in range(1,17):
+        KangarooDegrees.append(KangarooGraph.degree(node))
+
+    plt.figure()
+    plt.title('Histogram Undirected Kangaroo')
+    plt.hist(KangarooDegrees,bins = 9)
+    plt.show()
 
 main()
-
 
 
 
