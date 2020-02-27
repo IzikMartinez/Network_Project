e5dd744a20dc364f96900202a6ca0793663b96a5
Mara McGuire <maramcguire@ymail.com>
2020-02-27 01:24:26 -0600

    Added CreateNetwork Function and main function
    Signed-off-by: Mara McGuire <maramcguire@ymail.com>

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import networkx as nx
 import numpy as np
 import matplotlib.pyplot as plt
+import random
 
 
 # Create Network: This function is to create a network based on the provided information
@@ -27,7 +28,7 @@ def CreateNetwork (Graph,File,flag):
     nx.draw_networkx_nodes(Graph, pos, node_size=50)
     # Edges
     nx.draw_networkx_edges(Graph, pos, width=1)
-    plt.show()
+    #lt.show()
 
 # Main Function for the program.
 def main():
@@ -44,12 +45,15 @@ def main():
     KangaroonNetwork = CreateNetwork(KangarooGraph,KangarooFile,False)
 
     # Part A: Connected Component Analysis
+    # Connected Component Analysis of Bison Directed Graph
+    print("PART A:\n")
     print("Bison Directed Graph Connected Component Analysis",
           "\nWeakly connected: ", nx.is_weakly_connected(BisonGraph),
           "\nNumber of Weakly CCs: ", nx.number_weakly_connected_components(BisonGraph),
           "\nSize of largest CC: ", len(max(nx.weakly_connected_components(BisonGraph), key=len)),
           "\nSize of smallest CC: ", len(min(nx.weakly_connected_components(BisonGraph), key=len)))
 
+    # Connected Component Analysis of Kangaroo Undirected Graph
     print("\nKangaroo Undirected Graph Connected Component Analysis",
           "\nConnected: ", nx.is_connected(KangarooGraph),
           "\nNumber of CCs: ", nx.number_connected_components(KangarooGraph),
@@ -57,23 +61,92 @@ def main():
           "\nSize of smallest CC: ", len(min(nx.connected_components(KangarooGraph), key=len)))
 
     # Part B Computing Degrees and finding the Probability distribution
+    # Creation of an arrayList to store the degree for each node of Bison Network
     BisonDegrees = []
     for node in range(1,26):
         BisonDegrees.append(BisonGraph.degree(node))
 
-    plt.figure()
-    plt.title('Histogram Directed Bison')
+    # Creating a Histogram to plot the data of the degrees Bison Network
+    plt.figure(3)
+    plt.title('Part B: Histogram Directed Bison')
     plt.hist(BisonDegrees,bins = 17)
 
-
+    # Creation of an arrayList to store the degree for each node of Kangaroo Network
     KangarooDegrees = []
     for node in range(1,17):
         KangarooDegrees.append(KangarooGraph.degree(node))
 
-    plt.figure()
-    plt.title('Histogram Undirected Kangaroo')
+    # Creating a Histogram to plot the data of the degrees for Kangaroo Network
+    plt.figure(4)
+    plt.title('Part B: Histogram Undirected Kangaroo')
     plt.hist(KangarooDegrees,bins = 9)
+    #lt.show()
+
+    #Part C Find the Path between 2 abritrary vertices in the largest CC
+    Node1 = random.randrange(1,27,1)
+    Node2 = random.randrange(1,27,1)
+    while Node1 == Node2:
+        Node1 = random.randrange(1,27,1)
+
+    # I put a cutoff on the list of simple paths for now so I can atleast run something
+    # cut off is the act of only focusing on the paths <= 5
+    BisonPaths = list(nx.all_simple_paths(BisonGraph,Node1,Node2,cutoff = 5))
+    BisonLens = []
+
+    for node in range(0,len(BisonPaths)-1):
+        BisonLens.append(len(BisonPaths[node]))
+
+    plt.figure(5)
+    plt.title('Part C: Histogram Directed Bison Paths')
+    plt.hist(BisonLens, bins = 5)
+    #lt.show()
+
+    Node1 = random.randrange(1,17,1)
+    Node2 = random.randrange(1,17,1)
+    while Node1 == Node2:
+        Node1 = random.randrange(1,17,1)
+
+    KangarooPaths = list(nx.all_simple_paths(KangarooGraph,Node1,Node2,cutoff = 5))
+    KangarooLens = []
+
+    for node in range(0,len(KangarooPaths)-1):
+        KangarooLens.append(len(KangarooPaths[node]))
+
+    plt.figure(6)
+    plt.title('Part C: Histogram Undirected Kangaroo Paths')
+    plt.hist(KangarooLens, bins = 5)
+
     plt.show()
+
+    # Part D Find the Simple Circuits between 2 abritrary vertices in the largest CC
+    # This will Tell us how many simple circuits are in the Bison Network I can't do a cut of on this but
+    # I am very confident it work.
+    #print(len(list(nx.simple_cycles(BisonGraph))))
+
+    # SKIPPING PART D FOR NOW
+
+    # Part E Check if Eulerian, Find a Eulerian Path
+    print("\nPART E:")
+    print("\nDirected Bison Graph")
+    print("Euelerian: ", nx.is_eulerian(BisonGraph))
+    print("Has a Eulerian Path: ", nx.has_eulerian_path(BisonGraph))
+
+    print("\nUndirected Kangaroo Graph")
+    print("Euelerian: ", nx.is_eulerian(KangarooGraph))
+    print("Has a Eulerian Path: ", nx.has_eulerian_path(KangarooGraph))
+
+    # Part F: Convert to Matrix.
+    #I don't know if this covers everything?
+    BisonMatrix = nx.to_numpy_matrix(BisonGraph)
+    plt.matshow(BisonMatrix)
+    #plt.show()
+
+    KangarooMatrix = nx.to_numpy_matrix(KangarooGraph)
+    plt.matshow(KangarooMatrix)
+    #plt.show()
+    # Part G:
+
+
 
 main()
 
