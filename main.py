1c54628b5370c70246430f03ef40b341602ba03c
Mara McGuire <maramcguire@ymail.com>
2020-02-28 20:23:42 -0600

    Added CreateNetwork Function and main function
    Signed-off-by: Mara McGuire <maramcguire@ymail.com>

--- main.py
+++ main.py
@@ -28,7 +28,30 @@ def CreateNetwork (Graph,File,flag):
     nx.draw_networkx_nodes(Graph, pos, node_size=50)
     # Edges
     nx.draw_networkx_edges(Graph, pos, width=1)
-    #lt.show()
+    #plt.show()
+
+#A funciton to check is two graphs are isomorphic AKA equivalent
+def Compare(G1,G2,):
+    if nx.is_isomorphic(G1,G2):
+        print("These Graphs are Equivalent")
+    else:
+        print("These Graphs are NOT Equivalent")
+
+def Add10Edges(Graph,size):
+    for i in range(1,10):
+        x = random.randrange(1,size,1)
+        y = random.randrange(1,size,1)
+        while x == y:
+            x = random.randrange(1,size,1)
+        Graph.add_edge(x,y)
+
+def TreeOrForest(Graph):
+    if nx.is_tree(Graph):
+        print("The Minimum Spanning Tree Generated from this Network is a Tree")
+    elif nx.is_forest(Graph):
+        print("The Minimum Spanning Tree Generated from this Network is a Forest")
+    else:
+        print("Neither a Tree nor Forest")
 
 # Main Function for the program.
 def main():
@@ -59,6 +82,7 @@ def main():
           "\nNumber of CCs: ", nx.number_connected_components(KangarooGraph),
           "\nSize of largest CC: ", len(max(nx.connected_components(KangarooGraph), key=len)),
           "\nSize of smallest CC: ", len(min(nx.connected_components(KangarooGraph), key=len)))
+
 
     # Part B Computing Degrees and finding the Probability distribution
     # Creation of an arrayList to store the degree for each node of Bison Network
@@ -116,7 +140,7 @@ def main():
     plt.title('Part C: Histogram Undirected Kangaroo Paths')
     plt.hist(KangarooLens, bins = 5)
 
-    plt.show()
+    #plt.show()
 
     # Part D Find the Simple Circuits between 2 abritrary vertices in the largest CC
     # This will Tell us how many simple circuits are in the Bison Network I can't do a cut of on this but
@@ -143,8 +167,62 @@ def main():
 
     KangarooMatrix = nx.to_numpy_matrix(KangarooGraph)
     plt.matshow(KangarooMatrix)
-    plt.show()
-    # Part G:
+    #plt.show()
+
+    # Part G: Copy Largest CC comparing it to a copy and a slightly differnt CC
+    print("\nPart G:\n")
+    BisonN1 = nx.Graph()
+    LargestCCBison = list(max(nx.weakly_connected_components(BisonGraph), key=len))
+    for i in LargestCCBison:
+        BisonN1.add_edge(i,i+1)
+    BisonN2 = BisonN1.copy()
+
+    print("Is BisonN1 Equivalent to BisonN2?")
+    Compare(BisonN1,BisonN2)
+
+    print("\nIs BisonN1 Equivalent to N3?")
+    BisonN3 = BisonN2.copy()
+    Add10Edges(BisonN3,len(BisonN3))
+    Compare(BisonN1,BisonN3)
+
+    KangarooN1 = nx.Graph()
+    LargestCCKangaroo = list(max(nx.connected_components(KangarooGraph), key=len))
+    for i in LargestCCKangaroo:
+        KangarooN1.add_edge(i,i+1)
+    KangarooN2 = KangarooN1.copy()
+
+    print("\nIs KangarooN1 Equivalent to KangarooN2?")
+    Compare(KangarooN1,KangarooN2)
+
+    print("\nIs KangarooN1 Equivalent to N3?")
+    KangarooN3 = KangarooN2.copy()
+    Add10Edges(KangarooN3,len(KangarooN3))
+    Compare(KangarooN1,KangarooN3)
+
+    #Part H: Generate Minimum Spanning Tree
+    print("\nPart H:\n")
+
+    Kangaroo_MinTree = nx.minimum_spanning_tree(KangarooGraph)
+    print("~A Minimum Spanning Tree was created for the Undirected Kangaroo Graph~")
+    TreeOrForest(Kangaroo_MinTree)
+    x = 0
+    y = 0
+    while(not(Kangaroo_MinTree.has_edge(x,y))):
+        x = random.randrange(1,17,1)
+        y = random.randrange(1,17,1)
+        while x == y:
+            x = random.randrange(1,17,1)
+
+    print("\nAn edge from the spanning tree was removed")
+    Kangaroo_MinTree.remove_edge(x,y)
+    TreeOrForest(Kangaroo_MinTree)
+
+    #Part I: Dijkstra's Algorithm
+
+
+
+
+
 
 
 
